649224b28e912a9e2f65fe59c88695435e1c7ae6
E-Hussain06 <ehsanhussain991@gmail.com>
2017-01-16 21:22:02 +0000

    added arg catches

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 'use strict';
 
+var argv = require('yargs').argv;
 var gulp = require('gulp');
+
 var tasks = require('./lib/');
 
 gulp.task('post', function () {
-    var title = 'another test example';
+    var argTitle = argv.title || argv.t;
+    var title = (argTitle) ? argTitle : undefined;
+
     tasks.post(title);
 });
 
 gulp.task('page', function () {
-    var title = 'Page title example';
-    var slug = 'post-title-example';
+    var argTitle = argv.title || argv.t;
+    var argSlug = argv.slug || argv.s;
+    var slug =  (argSlug) ? argSlug : undefined;
+    var title = (argTitle) ? argTitle : undefined;
+
     tasks.page(title, slug);
 });
 
